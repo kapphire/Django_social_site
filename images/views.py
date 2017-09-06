b15fe3b6aa08c4968d31430889b2d44ef5bb3ffc
kapphire <ken.sapphire87@yahoo.com>
2017-09-06 14:42:51 +0200

    ajax sending func completed

--- images/views.py
+++ images/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.shortcuts import get_object_or_404
+
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from common.decorators import ajax_required
+
 from .forms import ImageCreateForm
+from .models import Image
 
 
 # Create your views here.
@@ -22,3 +29,26 @@ def image_create(request):
 		form = ImageCreateForm(data = request.GET)
 	return render(request, 'images/image/create.html', {'section' : 'images', 'form' : form})
 
+
+def image_detail(request, id, slug):
+	image = get_object_or_404(Image, id = id, slug = slug)
+	return render(request, 'images/image/detail.html', {'section' : 'images', 'image' : image})
+
+
+@ajax_required
+@login_required
+@require_POST
+def image_like(request):
+	image_id = request.POST.get('id')
+	action = request.POST.get('action')
+	if image_id and action:
+		try:
+			image = Image.objects.get(id = image_id)
+			if action == 'like':
+				image.users_like.add(request.user)
+			else:
+				image.users_like.remove(request.user)
+			return JsonResponse({'status' : 'ok'})
+		except:
+			pass
+	return JsonResponse({'status' : 'ko'})
